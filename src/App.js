4e9f7a5c609e6943211d73071422bf794207f8c0
rushilogicrays <rushi@logicrays.com>
2021-03-22 16:26:06 +0530

    calender

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./screens/Login";
 import ChangePassword from "./screens/changePassword";
 import Header from "./components/header";
 import TournamentsDetails from './screens/tournamentsDetails';
+import TournamentsOverview from "./screens/tournamentsOverview";
 
 const App = () => {
   return(
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/change_password" exact component={ChangePassword} />
         <Route path="/pandingtournaments" exact component={PandingTournaments} />
         <Route path="/tournaments_details" exact component={TournamentsDetails} />
+        <Route path="/tournaments_overview" exact component={TournamentsOverview} />
       </Switch>
   </BrowserRouter>
   )
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,17 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios';
 
-const Login = () => {
+const Login = (event) => {
+    const submitLogin = () => {
+        axios({
+            method: 'post',
+            url: 'http://143.110.254.46/poker/rest-auth/login/',
+            data: {
+                username : "poker",
+                password : "poker"
+            } 
+          })
+            .then(function (response) {
+              console.log("res ---->", response.data);
+            });
+        event.preventDefault();
+    }
     return (
         <div className="login-page-main">
             <div className="container">
                 <div className="login-form">
                     <Form className="form-field">
                         <h1> Login </h1>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group>
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="username" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
@@ -22,7 +37,7 @@ const Login = () => {
 
                         <div className="action-btn">
                             <Link to="/change_password">Forgot Password</Link>
-                            <Button variant="primary" type="submit">Submit</Button>
+                            <Button variant="primary" type="submit" onClick={e => submitLogin(e)}>Submit</Button>
                         </div>
                     </Form>
                 </div>
