da0cfef6913a898230f0742864a42bf7190ccda2
rushilogicrays <rushi@logicrays.com>
2021-03-22 12:44:58 +0530

    make a api configration

--- src/screens/PandingTournaments.js
+++ src/screens/PandingTournaments.js
@@ -2,90 +2,45 @@ import React, { useEffect, useState } from 'react';
 import './Style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import moment from 'moment'
+
 
 const PandingTournaments = () => {
-  const [form, setForm] = useState(false);
-  const [data, setData] = useState([
-    {
-      "id": 1,
-      "name": "rushi",
-      "lastName": "kantaria",
-      "email": "email",
-      "email": "email",
-      "email": "email",
-      "save": "save"
-    },
-    {
-      "id": 2,
-      "name": "mihir",
-      "lastName": "kantaria",
-      "email": "email",
-      "email": "email",
-      "email": "email",
-      "save": "save"
-    },
-    {
-      "id": 3,
-      "name": "zakir",
-      "lastName": "kantaria",
-      "email": "email",
-      "email": "email",
-      "email": "email",
-      "save": "save"
-    },
-    {
-      "id": 4,
-      "name": "deepkbhai",
-      "lastName": "kantaria",
-      "email": "email",
-      "email": "email",
-      "email": "email",
-      "save": "save"
-    }
-  ])
-  const deletitem = (slectedData) => {
-    let updatedData = [];
-    updatedData = data.map((item) => item.id !== slectedData.id && item);
-    setData(updatedData)
-  }
+  const [data, setData] = useState([])
+
   useEffect(() => {
     axios({
       method: 'get',
-      url: 'http://143.110.254.46:8084/api/pending-tournaments',
+      url: 'http://143.110.254.46:8084/poker/api/pending-tournaments',
     })
-    .then(function (response) {
-      console.log("resp ---->", response.data);
-    });
-  });
+      .then(function (response) {
+        setData(response.data);
+      });
+  }, []);
 
   return (
     <div className="App">
-        {data.map((item) => {
-          return(
-            <div>
-              {item && 
-                <div className="data-fatch">
-                  <p>{item.name}</p>
-                  <p>{item.lastName}</p>
-                  <p>{item.email}</p>
-                  <p>{item.email}</p>
-                  <p><button onClick={() => deletitem(item)}>Delelt</button><button onClick={() => setForm(true)}>edit</button></p>
-                </div>
-              }
-            </div>
-          )
-        })}
-        {form && <div style={{marginTop: 40}}>
-          <div style={{flexDirection: "row"}}>
-            <a style={{marginRight: 12}}>Name:</a>
-            <input type="text" name="title" />
+      <div className="data-fatch">
+        <p>Tournament ID</p>
+        <p>Start</p>
+        <p>End</p>
+        <p>Total Players</p>
+      </div>
+      {data.map((item) => {
+        return (
+          <div>
+            {item &&
+              <div style={{cursor: "pointer"}} onClick={() => console.log("here")} className="data-fatch">
+                <p>{"#" + item.external_id}</p>
+                <p>{moment(item.start_tournament).format("h:mm") + " - " + moment(item.start_tournament).format("DD.MM.YY")}</p>
+                <p>{moment(item.end_tournament).format("h:mm") + " - " + moment(item.end_tournament).format("DD.MM.YY")}</p>
+                <p>{item.total_players}</p>
+                {/* <p><button onClick={() => deletitem(item)}>Delelt</button><button onClick={() => setForm(true)}>edit</button></p> */}
+              </div>
+            }
           </div>
-          <div style={{flexDirection: "row", marginTop: 15}}>
-            <a style={{marginRight: 14}}>Email</a>
-            <input type="text" name="title" />
-          </div>
-          <button style={{marginTop: 15}} onClick={() => setForm(false)}>submit</button>
-        </div>}
+        )
+      })}
     </div>
   );
 }
